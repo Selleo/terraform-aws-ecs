22f723f76624988f9baf7adf0a2285df713d0d94
Bartłomiej Wójtowicz <wojtowicz.bartlomiej@gmail.com>
2023-03-12 16:01:52 +0100

    Add loadbalancer free service (#20)

--- examples/ec2-redis/main.tf
+++ examples/ec2-redis/main.tf
@@ -1,7 +1,7 @@
 resource "random_id" "example" {
   byte_length = 4
 
-  prefix = "static-"
+  prefix = "redis-"
 }
 
 module "info" {
@@ -10,7 +10,7 @@ module "info" {
 
   namespace = "selleo"
   stage     = "dev"
-  name      = "static-ports"
+  name      = "redis"
 }
 
 module "vpc" {
@@ -18,6 +18,12 @@ module "vpc" {
   version = "0.6.0"
 
   context = module.info.context
+}
+
+resource "aws_security_group" "lb" {
+  description = "No load balancer so we can define SG to create cluster"
+  vpc_id      = module.vpc.id
+  name        = "${random_id.example.hex}-lb"
 }
 
 module "cluster" {
@@ -29,7 +35,7 @@ module "cluster" {
   vpc_id               = module.vpc.id
   subnet_ids           = module.vpc.public_subnets
   instance_type        = "t3.nano"
-  lb_security_group_id = module.lb.security_group_id
+  lb_security_group_id = aws_security_group.lb.id
   allow_ssh            = true
 
   autoscaling_group = {
@@ -37,16 +43,6 @@ module "cluster" {
     max_size         = 5
     desired_capacity = 1
   }
-}
-
-module "lb" {
-  source  = "Selleo/backend/aws//modules/load-balancer"
-  version = "0.23.0"
-
-  name        = random_id.example.hex
-  vpc_id      = module.vpc.id
-  subnet_ids  = module.vpc.public_subnets
-  force_https = false
 }
 
 module "service" {
@@ -60,45 +56,21 @@ module "service" {
   cluster_id    = module.cluster.id
   desired_count = 1
 
-  image = "qbart/go-http-server-noop:latest"
+  image = "redis:7.0.9"
   port = {
-    host      = 8080
-    container = 4000
+    host      = 6379
+    container = 6379
   }
   limits = {
-    mem_min = 128
+    mem_min = 64
     mem_max = 256
     cpu     = 256
   }
-}
-
-resource "aws_security_group_rule" "allow_8080" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::/0"]
-  security_group_id = module.lb.security_group_id
-}
-
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = module.lb.id
-  port              = 8080
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = module.service.lb_target_group_id
-    type             = "forward"
-  }
+  create_alb_target_group = false
 }
 
 resource "null_resource" "output_pem" {
   provisioner "local-exec" {
     command = "echo '${module.cluster.private_key_pem}' > key.pem && chmod 0600 key.pem"
   }
-}
-
-output "lb_dns" {
-  value = "http://${module.lb.dns_name}"
 }
